93886646b7f40b087ade377363533f7b7e2e03fa
DeltaByte101 <notnoth43@gmail.com>
2021-04-04 11:48:22 +0700

    Delete another showpage and Complete Show1

--- webapp/routes/queryevent.js
+++ webapp/routes/queryevent.js
@@ -10,21 +10,27 @@ router.get('/event', function(req, res, next)
     connection.query('SELECT * FROM `showinfo`', function(err,result)
     {
         console.log("Result : ")  ;
+        let date = new Date()
+        let when = [] ;
         // console.log(result) ;
         result.forEach(res=>{
-            console.log(res)
-        })
-        res.render('event', { items:result });
+            console.log(new Date(res.Endate).getDate());
+            console.log(new Date(res.Endate).getMonth());
+            console.log(new Date(res.Endate).getFullYear());
+            when.push(`${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`) ;
+        });
+        
+        res.render('event', { items:result , date : when });
     });
 });
 
 router.get('/show1/:id', function(req, res, next)
 {
-    connection.query(`SELECT shi.*, z.ZoneName , z.ZonePrice FROM showinfo shi JOIN zone z ON(shi.Hallnumber=z.HallID) WHERE shi.Show_ID = ${req.params.id}`, function(err,result)
+    connection.query(`SELECT shi.*, z.ZoneName , z.ZonePrice , z.Capacity FROM showinfo shi JOIN zone z ON(shi.Hallnumber=z.HallID) WHERE shi.Show_ID = ${req.params.id}`, function(err,result)
     {  
         console.log(req.params.id) ;
-        console.log(result) ;
-        res.render('show1', { name:result[0] });
+        console.log(result[0].EndDate) ;
+        res.render('show1', { name:result });
     });
 });
 module.exports = router;
